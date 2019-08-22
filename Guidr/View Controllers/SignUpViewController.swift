cd2e0c6d6a97a5b731666f734c8d87d2f0538191
Jessie Griffin <i.jessie.me@icloud.com>
2019-08-21 23:17:57 -0400

    Add two outlets for the textfield and two actions for the buttons

--- Guidr/View Controllers/SignUpViewController.swift
+++ Guidr/View Controllers/SignUpViewController.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class SignUpViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func signUpButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func toLoginButtonPressed(_ sender: UIButton) {
+    }
     /*
     // MARK: - Navigation
 
