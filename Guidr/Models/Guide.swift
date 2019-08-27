2c4d2fe9fee62f08ab3fa5241f56f2981658e8fd
Stephanie <sbowl001@fiu.edu>
2019-08-28 00:47:23 +0800

    brainstorming for create user and create trip

--- Guidr/Models/Guide.swift
+++ Guidr/Models/Guide.swift
@@ -8,25 +8,66 @@
 
 import Foundation
 
-struct Guide:  Codable {
-    let id: Int 
+struct Guide:  Codable{
+    var id: Int?
     let name: String
     let age: Int
     let title: String
-    let tagline: String
+    let tagline: String?
     let yearsAsGuide: Int
-    let profilePic: URL
-    let coverPic: URL
-}
+    
+    init(id: Int, name: String, age: Int, title: String, tagline: String, yearsAsGuide: Int) {
+        self.id = id
+        self.name = name
+        self.age = age
+        self.title = title
+        self.tagline = tagline
+        self.yearsAsGuide = yearsAsGuide
+    }
+//    let profilePic: URL
+//    let coverPic: URL
+    
+//    enum CodingKeys: String, CodingKey {
+//        case id = "user_id"
+//    }
+//
+//
+//    init(from decoder: Decoder) throws {
+//        let values = try decoder.container(keyedBy: CodingKeys.self)
+//        id = try values.decode(Int.self, forKey: .id)
+//        self.name = name
+//        self.age = age
+//        self.title = title
+//        self.tagline = tagline
+//        self.yearsAsGuide = yearsAsGuide
+//
+//    }
+//
+//    func encode(to encoder: Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(id, forKey: .id)
+//}
 
-struct Trip: Codable {
-    let user_id: Int
+}
+struct Trip: Codable, Equatable {
+    var user_id: Int
     let title: String
-    let shortDescription: String
-    let isProfessional: Bool
+    let shortDescription: String?
+    let isProfessional: Bool?
     let type: Int
     let duration: Int
-    let distance: Int
+    let distance: Int?
     let date: String
-    let image: URL 
+//    let image: URL
+    
+    init(user_id: Int, title: String, shortDescription: String, isProfessional: Bool, type: Int, duration: Int, distance: Int, date: String) {
+        self.user_id = user_id
+        self.title = title
+        self.shortDescription = shortDescription
+        self.isProfessional = isProfessional
+        self.type = type
+        self.duration = duration
+        self.distance = distance
+        self.date = date
+    }
 }
--- Guidr/NetworkController/GuideController.swift
+++ Guidr/NetworkController/GuideController.swift
@@ -26,6 +26,7 @@ enum NetworkError: Error {
 
 class GuideController {
     var guide: [Guide] = []
+    var trip: [Trip]  = []
     
     var bearer: Bearer?
    
@@ -111,5 +112,109 @@ class GuideController {
  
     }
     
-   
+    func putUser(username: String, name: String, age: Int, title: String, yearsAsGuide: Int, completion: @escaping (NetworkError?) -> ()) {
+        
+        //how to initialize this?
+        let newUser = User(username: username)
+        let newGuide = Guide(name: name, age: age, title: title, yearsAsGuide: yearsAsGuide)
+       
+        
+        guard let bearer = self.bearer else {
+            completion(.noAuth)
+            return
+        }
+        
+        let putUserURL = baseURL.appendingPathComponent("auth/update")
+        var request = URLRequest(url: putUserURL)
+        request.httpMethod = HTTPMethod.put.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("\(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(newGuide)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding guide: \(error)")
+            completion(.otherError)
+        }
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 200 {
+                completion(.badAuth)
+                return
+            }
+            if let _ = error {
+                completion(.otherError)
+                return
+            }
+            
+            self.guide.append(newGuide)
+            
+            //Do I need to do decoder again for the new token?
+            guard let data = data else {
+                completion(.badData)
+                return
+            }
+            let decoder = JSONDecoder()
+            
+            do {
+             
+                self.bearer = try decoder.decode(Bearer.self, from: data)
+                
+            } catch {
+                NSLog("error decoding bearer object: \(error)")
+                completion(.noDecode)
+                return
+                
+            }
+            completion(nil)
+            } .resume()
+        
+    }
+    
+    func createTrip(user_id: Int, title: String, type: Int, duration: Int, date: String, completion: @escaping (NetworkError?) -> ()) {
+        
+        let newTrip = Trip(user_id: user_id, title: title, type: type, duration: duration, date: date)
+        guard let bearer = self.bearer else {
+            completion(.noAuth) //SLB
+            return
+        }
+        let postTripURL = baseURL.appendingPathComponent("trip")
+        var request = URLRequest(url: postTripURL)
+        request.httpMethod = HTTPMethod.post.rawValue
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Bearer \(bearer.token)", forHTTPHeaderField: "Authorization")
+        
+        
+    
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(newTrip)
+            request.httpBody = jsonData
+        } catch {
+            NSLog("Error encoding user object: \(error)")
+            completion(.otherError)
+        }
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            if let response = response as? HTTPURLResponse,
+                response.statusCode != 201 {
+                completion(.badAuth)
+                return
+            }
+            if let _ = error {
+                completion(.otherError)
+                return
+            }
+            guard let data = data else {
+                completion(.badData)
+                return
+            }
+            self.trip.append(newTrip)
+            completion(nil)
+            } .resume()
+    }
+        
 }
